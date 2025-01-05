952ff49ea3917d4dbc64577283e939d4e3708ba6
shalhan <shalhanradifan44@gmail.com>
2025-01-05 12:06:20 +0700

    add session under authcontroller

--- src/app.ts
+++ src/app.ts
@@ -1,42 +1,41 @@
 import express from 'express';
-import { authCtrl, transactionCtrl, transactionSvc } from './di';
+import { authCtrl, transactionCtrl } from './di';
 import readline from 'readline';
+import { AuthController } from './app/cli/AuthController';
 
 const app = express();
 const port = 3022;
 
 
 app.listen(port, () => {
-  console.log("app running...");
+  console.log("app running... available command (login, logout, deposit, transfer)");
   const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
   });
 
-  const session = {}
-
   const commands = {
       login: async (username: string) => {
         await authCtrl.login(username);
-        session["username"] = username; 
       },
       transfer: async (to: string, amount: number) => {
-        if (Object.keys(session).length == 0 || session["username"] == "") {
+        if (AuthController.session["username"] == "") {
           console.error('unauthorized');
           return;
         }
-        await transactionCtrl.transfer(session["username"], to, amount);
+        
+        await transactionCtrl.transfer(AuthController.session["username"], to, amount);
       },
       deposit: async (amount: number) => {
-        if (Object.keys(session).length == 0 || session["username"] == "") {
+        if (AuthController.session["username"] == "") {
           console.error('unauthorized');
           return;
         }
-        await transactionCtrl.deposit(session["username"], amount);
+        await transactionCtrl.deposit(AuthController.session["username"], amount);
       },
       logout: () => {
-          console.log(`Goodbye, ${session["username"]}!`);
-          Object.assign(session, {})
+          console.log(`Goodbye, ${AuthController.session["username"]}!`);
+          AuthController.session = {};
       },
       exit: () => {
         console.log("EXIT closing app...");
--- src/app/cli/AuthController.ts
+++ src/app/cli/AuthController.ts
@@ -1,6 +1,7 @@
 import * as Auth from '../../core/auth'
 
 export class AuthController {
+    public static session = {};
     private authService: Auth.Service;
     
     constructor(authService: Auth.Service) {
@@ -9,18 +10,25 @@ export class AuthController {
     
     public async login(username: string) {
         try {
+            if (AuthController.session != null && AuthController.session["username"] != undefined) {
+                console.error("you already logged in, please logout first")
+                return;
+            }
+
             const res = await this.authService.login(username);
+            AuthController.session["username"] = res.username;
+            
             console.log(`Hello, ${res.username}!`);
             console.log(`Your balance is $${res.balance}`);
+
             if (res.owedTo != "" && res.owedToAmount != 0) {
-            console.log(`Owed $${res.owedToAmount} to ${res.owedTo}`);
+                console.log(`Owed $${res.owedToAmount} to ${res.owedTo}`);
             }
             if (res.owedFrom != "" && res.owedFromAmount != 0) {
-            console.log(`Owed $${res.owedFromAmount} from ${res.owedFrom}`);
+                console.log(`Owed $${res.owedFromAmount} from ${res.owedFrom}`);
             }
         } catch (err) {
             console.error('error during login', err.message);
-            throw err;
         }
     }
 }
